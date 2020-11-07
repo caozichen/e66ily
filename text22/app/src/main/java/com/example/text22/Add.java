9c3cac33a394980115c28470079d5dffb2d14560
e66ily <zichencao522@126.com>
2020-11-08 02:29:14 +0800

    final2

---
+++ text22/app/src/main/java/com/example/text22/Add.java
@@ -0,0 +1,100 @@
+package com.example.text22;
+
+import android.content.ContentValues;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.lang.String;
+import android.content.SharedPreferences;
+
+public class Add extends AppCompatActivity implements OnClickListener{
+    String Title,Content,simpleDate;
+    Button ButtonAddCancel,ButtonAddSave;
+    EditText EditTextAddTitle,EditTextAddContent,EditTextAddAuthor;
+    String Author;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        ButtonAddCancel = (Button)findViewById(R.id.ButtonAddCancel);
+        ButtonAddSave = (Button)findViewById(R.id.ButtonAddSave);
+        EditTextAddContent = findViewById(R.id.EditTextAddContent);
+        EditTextAddTitle = findViewById(R.id.EditTextAddTitle);
+        EditTextAddAuthor = findViewById(R.id.EditTextAddAuthor);
+        ButtonAddCancel.setOnClickListener(this);
+        ButtonAddSave.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        MyDataBaseHelper dbHelper = new MyDataBaseHelper(this,"Note.db",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        switch (v.getId()) {
+            case R.id.ButtonAddSave:
+                Date date = new Date();
+                DateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");        //配置时间格式
+                simpleDate = simpleDateFormat.format(date);
+                ContentValues values = new ContentValues();
+                Title = String.valueOf(EditTextAddTitle.getText());         //获取需要储存的值
+                Content = String.valueOf(EditTextAddContent.getText());
+                Log.d("Title",Title);
+                if(Title.length()==0){               //标题为空给出提示
+                    Toast.makeText(this, "请输入一个标题", Toast.LENGTH_LONG).show();
+                }else {
+                    values.put("title", Title);
+                    values.put("content", Content);
+                    values.put("date", simpleDate);
+                    db.insert("Note", null, values);                 //将值传入数据库中
+                    Add.this.setResult(RESULT_OK, getIntent());
+                    Add.this.finish();
+                }
+
+
+                Author = String.valueOf(EditTextAddAuthor.getText());
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("author",Author);      //使用sharedperferences设置默认作者
+                editor.apply();
+                break;
+
+            case R.id.ButtonAddCancel:
+                Add.this.setResult(RESULT_OK,getIntent());
+                Add.this.finish();
+
+                break;
+        }
+
+
+    }
+}
